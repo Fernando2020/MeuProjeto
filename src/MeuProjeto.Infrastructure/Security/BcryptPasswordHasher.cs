8a5b205d545967a62bbf7fe74d00bf9010f6c873
Fernando Nogueira <fgeraldo11@gmail.com>
2025-07-27 21:36:05 -0300

    Sprint 4: Serviço Bcrypt para criptografar senha

---
+++ src/MeuProjeto.Infrastructure/Security/BcryptPasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using MeuProjeto.Core.Security;
+
+namespace MeuProjeto.Infrastructure.Security
+{
+    public class BcryptPasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
