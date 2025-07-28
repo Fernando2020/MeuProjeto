5a31b539a841d72712c9a57cb29c8fee65a90277
Fernando Nogueira <fgeraldo11@gmail.com>
2025-07-27 21:38:56 -0300

    Sprint 4: Excceção personalizada para domínio

---
+++ src/MeuProjeto.Core/Exceptions/MyDomainException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MeuProjeto.Core.Exceptions
+{
+    public class MyDomainException : BaseException
+    {
+        public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+        public MyDomainException(string message) : base(message) { }
+    }
+}
